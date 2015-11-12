80a6359f77c3f69914d51b2a09c19edcca335331
Matt Morris <mcm0027@yahoo.com>
2015-11-12 12:21:56 -0600

    add new controller and rootscope to earnings variables

--- script.js
+++ script.js
@@ -2,35 +2,39 @@ angular.module("myApp", ['ngRoute'])
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
       templateUrl : './home.html',
-      controller : 'OneController'
+      controller : 'HomeController'
     }).when('/new_meal', {
       templateUrl : './new_meal.html',
-      controller : 'OneController'
+      controller : 'HomeController'
     }).when('/my_earnings', {
       templateUrl : './my_earnings.html',
-      controller : 'OneController'
+      controller : 'EarningsController'
     }).otherwise('/' , {
       templateUrl : './home.html',
-      controller : 'OneController'
+      controller : 'HomeController'
     });
   }])
-  .controller('OneController', function ($scope) {
-    $scope.mealPrice;
-    $scope.taxRate;
-    $scope.tipRate; 
-    $scope.customerCharges = {};
+  .controller('HomeController', function ($scope, $rootScope) {
     
-    $scope.earnings = {};
-    
-    $scope.i = 0;
-    $scope.showError = false;
-    
-    $scope.customerCharges.subTotal = 0;
-    $scope.customerCharges.tip = 0;
-    $scope.customerCharges.total = 0;
-    $scope.earnings.tipTotal = 0;
-    $scope.earnings.mealCount = 0;
-    $scope.earnings.average = 0;
+
+      $scope.mealPrice;
+      $scope.taxRate;
+      $scope.tipRate; 
+      $scope.customerCharges = {};
+
+      $rootScope.earnings = {};
+
+      $scope.i = 0;
+      $scope.showError = false;
+
+      $scope.customerCharges.subTotal = 0;
+      $scope.customerCharges.tip;
+      $scope.customerCharges.total = 0;
+
+      $rootScope.earnings.tipTotal;
+      $scope.earnings.mealCount;
+      $scope.earnings.average;
+
 
     $scope.submit = function () {
       $scope.showError = true;
@@ -39,27 +43,27 @@ angular.module("myApp", ['ngRoute'])
       $scope.customerCharges.total = $scope.customerCharges.subTotal + $scope.customerCharges.tip;
       
       if ($scope.i === 0) {
-      $scope.earnings.tipTotal = $scope.customerCharges.tip;
+      $rootScope.earnings.tipTotal = $scope.customerCharges.tip;
       } else { 
-        $scope.earnings.tipTotal += $scope.customerCharges.tip;
+        $rootScope.earnings.tipTotal += $scope.customerCharges.tip;
     };
       $scope.i++;
       $scope.earnings.mealCount = $scope.i;
-      $scope.earnings.average = $scope.earnings.tipTotal / $scope.earnings.mealCount;
+      $scope.earnings.average = $rootScope.earnings.tipTotal / $scope.earnings.mealCount;
     
     
   };
     
     $scope.cancel = function() {
-      document.getElementById("enter").reset();
+      $scope.getElementById("enter").reset();
       $scope.form.$submitted = false;
       $scope.showError = false;
       $scope.mealPrice = '';
       $scope.taxRate = '';
       $scope.tipRate = ''; 
     };
-    
-    $scope.reset = function() {
+  
+  $scope.reset = function() {
       document.getElementById("enter").reset();
       $scope.mealPrice = '';
       $scope.taxRate = '';
@@ -67,12 +71,16 @@ angular.module("myApp", ['ngRoute'])
       $scope.customerCharges.subTotal = 0;
       $scope.customerCharges.tip = 0;
       $scope.customerCharges.total = 0;
-      $scope.earnings.tipTotal = 0;
+      $rootScope.earnings.tipTotal = 0;
       $scope.earnings.mealCount = 0;
       $scope.i = 0;
       $scope.earnings.average = 0;
       $scope.form.$submitted = false;
       $scope.showError = false;
-
-    };
+  };
+  
+})
+.controller('EarningsController', function ($scope, $rootScope) {
+  $rootScope.earnings.tipTotal
+  
 });
